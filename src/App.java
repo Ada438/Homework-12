c05279fb8a4a294d4baa2fa1f170dd7b7ef9d248
Ada438 <ada438438@gmail.com>
2025-04-04 15:36:49 +0300

    HW-13

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 class Book {
     private String title;
     private Author author;
@@ -22,6 +23,26 @@ class Book {
     public void setYear(int year){
         this.year=year;
     }
+    @Override
+    public String toString() {
+        return "Книга: " + title + ", автор: " + author.toString() + ", год: " + year;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+        Book book = (Book) other;
+        return year == book.year &&
+                Objects.equals(title, book.title) &&
+                Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, year);
+    }
+
 }
 class Author {
     private String authorName;
@@ -38,7 +59,25 @@ class Author {
     public String getAuthorSurname(){
         return authorSurname;
     }
+    @Override
+    public String toString() {
+        return authorName + " " + authorSurname;
+    }
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+        Author author = (Author) other;
+        return Objects.equals(authorName, author.authorName) &&
+                Objects.equals(authorSurname, author.authorSurname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(authorName, authorSurname);
+    }
 }
+
 public class App {
     public static void main(String[] args) {
         Author author1 = new Author("Джоан", "Роулинг");
@@ -46,10 +85,11 @@ public class App {
 
         Book book1 = new Book("Гарри Поттер", author1, 1997);
         Book book2 = new Book("Оливер Твист", author2, 1838);
-        book2.setYear(1840);
 
-        System.out.println("Книга: " + book1.getTitle() + ", автор: " + book1.getAuthor().getAuthorName() + " " + book1.getAuthor().getAuthorSurname() + ", год: " + book1.getYear());
-        System.out.println("Книга: " + book2.getTitle() + ", автор: " + book2.getAuthor().getAuthorName() + " " + book2.getAuthor().getAuthorSurname() + ", год: " + book2.getYear());
+        System.out.println(book1);
+        System.out.println(book2);
+
+        System.out.println("Книги равны? " + book1.equals(book2));
     }
 }
 
